0af2863e06189c0b681384655d87242137d78cae
Eysvid <111803923+Eysvid@users.noreply.github.com>
2023-12-22 14:31:25 +0300

    Add files via upload

--- src/js/modules/scroll-opacity.js
+++ src/js/modules/scroll-opacity.js
@@ -1,20 +1,20 @@
-function scroolOpacity () {
- const header = document.querySelector(".header");
-    let prevScrollPos = window.pageYOffset;
-    
-    window.onscroll = function() {
-      const scrollPos = window.pageYOffset || document.documentElement.scrollTop;
-      const maxScroll = header.offsetHeight;
-      
-      if (prevScrollPos > scrollPos) {
-        const opacity = (maxScroll - scrollPos) / maxScroll;
-        header.style.opacity = opacity;
-      } else {
-        const opacity = (maxScroll - scrollPos) / maxScroll*0.3;
-        header.style.opacity = opacity;
-      }
-      prevScrollPos = scrollPos;
+function scroolOpacity() {
+  const header = document.querySelector(".header");
+  let prevScrollPos = window.pageYOffset;
+
+  window.onscroll = function () {
+    const scrollPos = window.pageYOffset || document.documentElement.scrollTop;
+    const maxScroll = header.offsetHeight;
+
+    if (prevScrollPos > scrollPos) {
+      const opacity = (maxScroll - scrollPos) / maxScroll;
+      header.style.opacity = opacity;
+    } else {
+      const opacity = (maxScroll - scrollPos) / maxScroll * 0.3;
+      header.style.opacity = opacity;
     }
+    prevScrollPos = scrollPos;
+  }
 }
 
 export default scroolOpacity;
