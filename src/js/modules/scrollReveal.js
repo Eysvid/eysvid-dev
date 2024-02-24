878bba761d1030c77c1c5e09de0eb1eea55b91ac
Eysvid <111803923+Eysvid@users.noreply.github.com>
2024-02-24 18:04:52 +0300

    Add files via upload

--- src/js/modules/scrollReveal.js
+++ src/js/modules/scrollReveal.js
@@ -2,31 +2,43 @@ import ScrollReveal from 'scrollreveal';
 
 // Базовые настройки
 ScrollReveal({
-	distance: '100px',
-	duration: 2500
+  distance: '100px',
+  duration: 2500,
 });
 
 function scrollRevealFunc() {
-	ScrollReveal().reveal(`.home__projects`, {
-		distance: '0px',
-		opacity: 0,
-	});
+  ScrollReveal().reveal(`.home__projects, .block--visible`, {
+    distance: '0px',
+    opacity: 0,
+  });
 
-	ScrollReveal().reveal(`.home__avatar, .discover__picture-hint, .footer__title`, {
-		origin: 'top',
-	});
+  ScrollReveal().reveal(
+    `.home__avatar, .discover__picture-hint, .footer__title`,
+    {
+      origin: 'top',
+    }
+  );
 
-	ScrollReveal().reveal(`.home__title, .footer__subtitle`, {
-		origin: 'left',
-	});
+  ScrollReveal().reveal(
+    `.home__title, .home__block-item--left, .footer__subtitle`,
+    {
+      origin: 'left',
+    }
+  );
 
-	ScrollReveal().reveal(`.home__subtitle, .footer__email`, {
-		origin: 'right',
-	});
+  ScrollReveal().reveal(
+    `.home__subtitle, .home__block-item--right, .footer__email`,
+    {
+      origin: 'right',
+    }
+  );
 
-	ScrollReveal().reveal(`.home__skils, .footer__socials`, {
-		origin: 'bottom',
-	});
+  ScrollReveal().reveal(
+    `.home__skils,  .home__block--bottom, .footer__socials`,
+    {
+      origin: 'bottom',
+    }
+  );
 }
 
 export default scrollRevealFunc;
