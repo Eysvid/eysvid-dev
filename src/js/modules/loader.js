2630b6c3c17fb83f8b9f7c43dd0e6c0490e0d284
Eysvid <111803923+Eysvid@users.noreply.github.com>
2024-04-09 20:23:32 +0300

    Add files via upload

--- src/js/modules/loader.js
+++ src/js/modules/loader.js
@@ -1,10 +1,10 @@
 function loader() {
-    const loader = document.querySelector('.loader-wrapper');
-    const video = document.querySelector('.header__video-src');
+  const loader = document.querySelector('.loader-wrapper');
+  const video = document.querySelector('.header__video-src');
 
-    video.addEventListener('loadeddata', () => {
-        loader.classList.add('loader-wrapper--hidden');
-    });
+  video.addEventListener('loadeddata', () => {
+    loader.classList.add('loader-wrapper--hidden');
+  });
 }
 
 export default loader;
